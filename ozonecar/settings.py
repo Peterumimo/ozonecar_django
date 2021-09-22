3c2a0982d41cb1d666dfed178300189ba3980429
peterumimo <peterumimo2@gmail.com>
2021-09-22 16:20:14 +0100

    Completed Team Model

--- ozonecar/settings.py
+++ ozonecar/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'ozonecar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',
+        'HOST': 'localhost',
     }
 }
 
@@ -124,6 +127,10 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'ozonecar/static',
 ]
+
+#Media Settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
